af2e5f23ab80b5f0e38715e7b6ec43f881ddb481
curso <curso@curso.com>
2022-07-11 20:47:12 -0300

    resolução

--- app/src/main/java/br/com/impacta/myapplication/Main.kt
+++ app/src/main/java/br/com/impacta/myapplication/Main.kt
@@ -1,6 +1,9 @@
 package br.com.impacta.myapplication
 
 fun main() {
+    var valorProduto = 100.0
+    var valorDesconto = 25.0
 
-    println("Olá")
+    var valorFinal = valorProduto * (1 - valorDesconto / 100.0)
+    println("O valor final do produto é ${valorFinal}")
 }
